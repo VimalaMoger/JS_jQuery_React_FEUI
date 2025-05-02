ae957a7e6e23bc928bf449284536bf0a20ec122e
VimalaMoger <mgrvmla1@gmail.com>
2025-05-02 11:55:02 -0500

    Added jQuery code

---
+++ README.md
@@ -0,0 +1,55 @@
+# Project goal
+
+I started with ***JavaScript***  focusing on common elements on html DOM.
+
+Currently working on _jQuery_ using css styles
+The ready event 
+    
+`$(document).ready(function(){`
+     
+
+  * Using key html tags to modify the contents within the document page, animation, X and Y coordinates
+> 
+## Contents  
+#### Libraries
+
+```js
+ - Included jQuery library from Google CDN
+
+ ```
+ 
+### Libraries and versions
+
+```
+| Library    | Version       | 
+|------------|---------------|
+| JavaScript |ECMAScript/ES14|                
+| jQuery     | 3.7.1         | 
+| React      | 19.1          |    
+| jQueryUI   | 1.14.1        |
+| Git        | 2.49.0        |
+```
+
+## Tasks
+
+- [x] JavaScript
+- [] jQuery
+- [] react
+
+## Images
+<!-- ![My image](images/img1.jpg)  -->
+
+
+**Tip:**
+- Other option to call ``jQuery(function($) {`` a ready function(shorter method) after the document is finished loading
+-  Always keep Document ready event block inside `<script>...</script>` 
+-  Always keep this script tag either inside the `<head>...</head>` or at the bottom of the page before closing `<body>` tag
+
+## References
+<details>
+
+  [StackOverflowQ/A](https://stackoverflow.com  "Answers")<br>
+  [Medium](https://medium.com)<br>
+  [Tutorialspoint](https://www.tutorialspoint.com)
+</details>
+
---
+++ jquery/jquery_functions.js
@@ -0,0 +1,176 @@
+$(document).ready(function(){
+    $("button").dblclick(function(){                //html elements with hide function
+        //$("h1").hide();
+        //$("p").hide();
+        //$("#heading2").hide();
+        //$("*").hide();//all the elements disappears
+        //$(this).hide(); //hide button itself
+        $(":button").hide(); //hide the button 
+    });
+    
+    $("p.line2").html("This line will show or hide after the click");
+    $("ul li:first").css("background-color", "yellow");
+    //$("[href]").html("Welcome to Google page");
+    $("tr:odd").css("color", "grey");
+    //$("a[target =='_blank']").css("color", "green");
+    
+    function message(){
+      alert("You have visited Milk products");
+    }
+
+    //Single event
+    //$("#milk").mouseup(message);  //mouseup
+    
+    //Adding multiple in jQuery
+    $("#milk").on({
+         mouseenter: function(){
+             $(this).css("background-color", "yellow");
+             $(this).click(function(){
+                alert("The color is changed");
+             })
+         },
+         mouseleave: function(){
+             $(this).css("background-color", "pink");
+         },
+         click: function(){
+             $(this).css("background-color", "cyan");
+         }
+    });
+
+    //hide - show a text
+    $("#hidden").click(function(){
+        $(".line2").hide(1000);
+    });
+
+    $("#visible").click(function(){
+        $(".line2").show(1000);
+    });
+
+
+    //toggle an element
+    $("#btn").click(function(){
+        $(".line3").toggle(1000);
+    });
+
+    //FadeIn - div children
+    $("#b1").click(function(){
+        $("#one").fadeIn();
+        $("#one > #two").fadeIn(4000);
+        $("#two > #three").fadeIn(6000);
+        $("#three > #four").fadeIn(8000);
+    });
+
+    //FadeOut in opposite direction
+     $("#b2").click(function(){
+        $("#one").fadeOut(8000);
+        $("#one > #two").fadeOut(6000);
+        $("#two > #three").fadeOut(4000);
+        $("#three > #four").fadeOut();
+    }); 
+
+    //Fade Toggle
+    $("#b3").click(function(){
+        $("#one").fadeToggle(8000);
+        $("#one > #two").fadeToggle(6000);
+        $("#two > #three").fadeToggle(4000);
+        $("#three > #four").fadeToggle();
+    }); 
+
+    //FadeTo with time and Opacity
+    $("#b4").click(function(){
+        $("#one").fadeTo(8000, 0.90);
+        $("#one > #two").fadeTo(6000, 0.60);
+        $("#two > #three").fadeTo(4000, 0.90);
+        $("#three > #four").fadeTo(9000, 0.99);
+    });
+
+    //slides down
+     $("#topbar").click(function(){
+        $("#panel").slideDown(1000);
+    });
+    //slides up
+    $("#topbar").click(function(){
+        $("#panel").slideUp(1000);
+    });
+    //Toggles
+    $("#topbar").click(function(){
+        $("#panel").slideToggle(1000);
+    }); 
+    //jQuery chaining
+    $("#topbar").click(function(){
+        $("#panel").css("background","green")
+        .slideUp(1000).slideDown(1000);
+    });
+
+    //draggable
+    $("#drag").click(function(){
+        $(function(){
+            $("#drag").draggable();
+        });
+    });
+
+    //Accordian menu
+    $(function(){
+        $("#accordion").accordion();
+    });
+
+    //jQuery get content
+    $("#button1").click(function(){
+        alert($("#milklist").text());
+    })
+    $("#button2").click(function(){
+        alert($("#milklist").html());
+    })
+
+    //Animate square using left, opacity, height properties
+     $("#square").click(function(){
+        $("#square").animate({left: '800px', opacity:0.25,height:"toggle", width:'60px'}, 1000);
+    }) 
+    //Animate to bigger size
+    $("#square").click(function(){
+        $("#square").animate({left: '800px', opacity:0.25,height:"+=200px", width:"+=200px"}, 1000);
+    }) 
+
+    //Queue functionality
+    $("#square").click(function(){
+        $("#square").animate({left: '400px', opacity:0.25}, 1000);
+        $("#square").animate({left: '600px', opacity:0.25}, 1000);
+        $("#square").animate({left: '800px', opacity:0.25}, 1000);
+    }); 
+
+    //Stop the animation
+    $("#square").click(function(){
+        $("#square").animate({left: '1500px', opacity:0.25}, 5000);
+    });
+    $(".stop").click(function(){
+        $("#square").stop();
+            alert("The square stopped rolling!")
+
+    }); 
+
+    //console output showing x and y co-ordinates values
+    document.addEventListener('mousemove', function(event) {
+     console.log('Mouse X:', event.clientX, 'Mouse Y:', event.clientY);
+     //console.log('Mouse X:', event.pageX, 'Mouse Y:', event.pageY);
+     const coordinates = document.getElementById('coordinates');
+    });
+
+    //x and y co-ordinates display on html document on browser
+     document.addEventListener('mousemove', function(event) {
+         const x = event.clientX;
+         const y = event.clientY;
+         coordinates.textContent = `Mouse X: ${x}, Mouse Y: ${y}`;
+     });
+
+
+     //animation : mousemove event where the text moves with mouse movement
+     const follower = document.querySelector('#heading2');
+
+     document.addEventListener('mousedown', function(event) {
+         const x = event.clientX;
+         const y = event.clientY;
+         follower.style.left = `${x}px`;
+         follower.style.top = `${y}px`;
+     });     
+      
+});
